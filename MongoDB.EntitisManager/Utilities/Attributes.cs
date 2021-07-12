81e9bd27951e860c0ba34853615cc181315a5ce9
thestormshadow <javiervargasruiz94@gmail.com>
2021-07-12 01:19:51 -0500

    CollectionName
    CollectionName

--- MongoDB.EntitisManager/Utilities/Attributes.cs
+++ MongoDB.EntitisManager/Utilities/Attributes.cs
@@ -44,18 +44,19 @@ namespace MongoDB.EntitiesManager
     /// <summary>
     /// Use this attribute to specify the database to store this IEntity in.
     /// </summary>
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
     public class ForeignField : Attribute
     {
         public string Name { get; }
-        public string TableName { get; }
+        public string CollectionName { get; }
         public bool IsFillAutomaticDetails { get; }
 
         /// <param name="name">The name you want to use for the collection</param>
-        public ForeignField(string name, string tableName = "", bool isFillAutomaticDetails = false)
+        public ForeignField(string name, string collectionName = "", bool isFillAutomaticDetails = false)
         {
             if (string.IsNullOrEmpty(name)) throw new ArgumentNullException("Name empty");
             Name = name;
-            TableName = tableName;
+            CollectionName = collectionName;
             IsFillAutomaticDetails = isFillAutomaticDetails;
         }
     }
